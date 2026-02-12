f3c9fcd4a9ca1aea7f2cf1e710d1aed5d153c798
paulthanksgiving <paulthanksgiving2@gmail.com>
2026-02-12 13:52:16 +0100

    hooks

---
+++ src/hooks/useRevealOnScroll.ts
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+
+export const useRevealOnScroll = () => {
+  useEffect(() => {
+    const nodes = document.querySelectorAll<HTMLElement>('.reveal')
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('revealed')
+          }
+        })
+      },
+      {
+        threshold: 0.2,
+      },
+    )
+
+    nodes.forEach((node) => observer.observe(node))
+
+    return () => {
+      nodes.forEach((node) => observer.unobserve(node))
+      observer.disconnect()
+    }
+  }, [])
+}
